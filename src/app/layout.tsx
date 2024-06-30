6bd2eef320fe29bed78a96738ac26538cfd1d3a4
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-06-30 22:16:28 +0900

    chore: Update RootLayout component to use GlobalStyles.scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import React from "react";
+import '@styles/GlobalStyles.scss';
 
-import "@styles/GlobalStyles.scss";
-
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="ja">
-			<body>{children}</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="ja">
+            <body>{children}</body>
+        </html>
+    );
 }
