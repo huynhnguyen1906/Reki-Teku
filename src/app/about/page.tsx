804b1277641fdd922f2978e0e17bd0843ef18ae6
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-06-30 22:26:35 +0900

    chore: Refactor About page component to use single quotes for import statement

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import NavBar from "@/components/NavBar/NavBar";
+import NavBar from '@/components/NavBar/NavBar';
 
 export default function About() {
-	return (
-		<div>
-			<NavBar />
-			<h1>About</h1>
-		</div>
-	);
+    return (
+        <div>
+            <NavBar />
+            <h1>About</h1>
+        </div>
+    );
 }
