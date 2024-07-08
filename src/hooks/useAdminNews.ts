df4e0d77339f24cf784f70f1eff0667434461bcd
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-08 16:56:18 +0900

    Refactor useAdminNews hook to remove unnecessary revalidation options

--- src/hooks/useAdminNews.ts
+++ src/hooks/useAdminNews.ts
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useAdminNews = () => {
-    const { data, error, mutate } = useSWR('/api/news-admin-view', fetcher, {
+    const { data, error } = useSWR('/api/news-admin-view', fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false,
