6a67effc64e2a9609a229411475437791099b533
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-09 11:03:30 +0900

    Refactor handleSend function to update news data after successful submission

--- src/utils/handleSaveNews.ts
+++ src/utils/handleSaveNews.ts
@@ -5,6 +5,7 @@ import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 import { storage } from '@/lib/firebase';
 import { convertToWebP } from '@/utils/changeTypeImg';
 import { mutate } from 'swr';
+
 export const handleSend = async (
     { editor, activeButton, router }: HandleSendParams,
     setLoading: (loading: boolean) => void,
@@ -44,7 +45,16 @@ export const handleSend = async (
 
         if (response.status === 200) {
             toast.success('記事の送信が完了しました');
-            mutate('/api/news-admin-view', undefined, true);
+
+            mutate(
+                '/api/news-admin-view',
+                async () => {
+                    const updatedNewsResponse = await axios.get('/api/news-admin-view');
+                    return updatedNewsResponse.data;
+                },
+                false,
+            );
+
             router.push('/admin/news');
         } else {
             toast.error('記事の送信に失敗しました');
