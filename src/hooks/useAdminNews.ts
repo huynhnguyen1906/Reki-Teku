7688f7b99d805a938e864aec68495c9d01b20711
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-09 10:51:36 +0900

    Refactor response headers in GET route to disable caching

--- src/hooks/useAdminNews.ts
+++ src/hooks/useAdminNews.ts
@@ -4,10 +4,11 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useAdminNews = () => {
     const { data, error } = useSWR('/api/news-admin-view', fetcher, {
-        revalidateIfStale: false,
+        revalidateIfStale: true,
         revalidateOnFocus: true,
         revalidateOnReconnect: true,
-        refreshInterval: 0,
+        refreshInterval: 30000,
+        dedupingInterval: 0,
     });
 
     return {
