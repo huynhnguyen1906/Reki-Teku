cad78591535421951b8d79c6fdd99dd74e43de8e
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-13 01:11:43 +0900

    refactor: Update MainVisual component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import MainLayout from '@/components/MainLayout';
 import Style from '@styles/appStyles/Index.module.scss';
+import picture from '../../public/images/blog_osaka/picture_03.webp';
+import mask from '../../public/images/firstviewMask.png';
 
 export default function Home() {
     return (
         <>
             <MainLayout>
                 <div className={Style.MainVisual}>
-                    <div className={Style.imgDiv}></div>
+                    <div
+                        className={Style.imgDiv}
+                        style={{
+                            backgroundImage: `url(images/blog_osaka/picture_03.webp)`,
+                            WebkitMaskImage: `url(images/firstviewMask.png)`,
+                            maskImage: `url(images/firstviewMask.png`,
+                        }}
+                    ></div>
                 </div>
             </MainLayout>
         </>
