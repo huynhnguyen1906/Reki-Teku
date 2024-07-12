5a0f0cafd8676bf5a3509c9c7284254202e3a66a
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-13 02:27:04 +0900

    refactor: Update MainVisual component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
 import MainLayout from '@/components/MainLayout';
 import Style from '@styles/appStyles/Index.module.scss';
-
+import VisualContent from '@/components/Index/VisualContent';
 export default function Home() {
-    const picture = '/images/blog_osaka/picture_03.webp';
-    const mask = '/images/firstviewMask.png';
-
     return (
-        <>
-            <MainLayout>
-                <div className={Style.MainVisual}>
-                    <div
-                        className={Style.imgDiv}
-                        style={{
-                            backgroundImage: `url(${picture})`,
-                            WebkitMaskImage: `url(${mask})`,
-                            maskImage: `url(${mask})`,
-                        }}
-                    ></div>
-                </div>
-            </MainLayout>
-        </>
+        <MainLayout>
+            <div className={Style.MainVisual}>
+                <VisualContent />
+            </div>
+        </MainLayout>
     );
 }
---
+++ src/components/Index/VisualContent.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useLayoutEffect } from 'react';
+import Style from '@styles/componentsStyles/Index/VisualContent.module.scss';
+
+export default function VisualContent() {
+    useLayoutEffect(() => {
+        const updateClipPath = () => {
+            const imgDiv = document.getElementById('imgDiv');
+            if (imgDiv) {
+                const width = window.innerWidth;
+                const height = window.innerHeight;
+                const path = `M1 2.5V${height - 50}C${width * 0.026} ${height - 36.333} ${width * 0.087} ${height} ${
+                    width * 0.125
+                } ${height}C${width * 0.163} ${height} ${width * 0.271} ${height - 10} ${width * 0.297} ${
+                    height - 20
+                }C${width * 0.353} ${height - 31} ${width * 0.441} ${height - 104.5} ${width * 0.559} ${
+                    height - 104.5
+                }C${width * 0.672} ${height - 104.5} ${width * 0.773} ${height + 100} ${width * 1.3} ${
+                    height - 224.5
+                }C${width * 0.903} ${height - 223.3} ${width * 0.983} ${height - 199.667} ${width} ${height - 188}V0H0`;
+                imgDiv.style.clipPath = `path('${path}')`;
+            }
+        };
+
+        updateClipPath();
+        window.addEventListener('resize', updateClipPath);
+
+        return () => window.removeEventListener('resize', updateClipPath);
+    }, []);
+
+    return <div id="imgDiv" className={Style.imgDiv}></div>;
+}
