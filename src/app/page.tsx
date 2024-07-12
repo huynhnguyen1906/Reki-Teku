5b889deb7c0118c5932ba03c26d8971419566b42
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-13 01:17:52 +0900

    refactor: Update MainVisual component in Home page
    - Removed unnecessary background image and mask from the MainVisual component in the Home page.
    - Updated CSS styles in Index.module.scss to remove unused properties and improve responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,21 @@ import MainLayout from '@/components/MainLayout';
 import Style from '@styles/appStyles/Index.module.scss';
 
 export default function Home() {
+    const picture = '/images/blog_osaka/picture_03.webp';
+    const mask = '/images/firstviewMask.png';
+
     return (
         <>
             <MainLayout>
                 <div className={Style.MainVisual}>
-                    <div className={Style.imgDiv}></div>
+                    <div
+                        className={Style.imgDiv}
+                        style={{
+                            backgroundImage: `url(${picture})`,
+                            WebkitMaskImage: `url(${mask})`,
+                            maskImage: `url(${mask})`,
+                        }}
+                    ></div>
                 </div>
             </MainLayout>
         </>
