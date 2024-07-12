9ad2aca2d57f2d93181eb8ea4cd963b20d4338f2
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-13 03:33:02 +0900

    refactor: Update MainVisual component in Home page
    - Removed unnecessary background image and mask from the MainVisual component in the Home page.
    - Updated CSS styles in Index.module.scss to remove unused properties and improve responsiveness.

--- src/components/Index/VisualContent.tsx
+++ src/components/Index/VisualContent.tsx
@@ -8,16 +8,29 @@ export default function VisualContent() {
             const imgDiv = document.getElementById('imgDiv');
             if (imgDiv) {
                 const width = window.innerWidth;
-                const height = window.innerHeight;
-                const path = `M0 2.5V${height - 50}C${width * 0.026} ${height - 36.333} ${width * 0.087} ${height} ${
-                    width * 0.125
-                } ${height}C${width * 0.163} ${height} ${width * 0.271} ${height - 10} ${width * 0.297} ${
-                    height - 20
-                }C${width * 0.353} ${height - 31} ${width * 0.441} ${height - 104.5} ${width * 0.559} ${
-                    height - 104.5
-                }C${width * 0.672} ${height - 104.5} ${width * 0.773} ${height + 100} ${width * 1.3} ${
-                    height - 224.5
-                }C${width * 0.903} ${height - 223.3} ${width * 0.983} ${height - 199.667} ${width} ${height - 188}V0H0`;
+                const height = window.innerHeight - 60;
+
+                let path;
+                if (width < 912) {
+                    path = `M0 ${height - 75.5}V0H${width}V${height - 100}C${width * 0.85} ${height} ${width * 0.65} ${
+                        height - 50
+                    } ${width * 0.5} ${height - 75.5}C${width * 0.35} ${height - 100} ${width * 0.15} ${height} 0.5 ${
+                        height - 75.5
+                    }Z`;
+                } else {
+                    path = `M0 2.5V${height - 50}C${width * 0.026} ${height - 36.333} ${width * 0.2} ${height} ${
+                        width * 0.195
+                    } ${height - 3}C${width * 0.2} ${height} ${width * 0.26} ${height - 0} ${width * 0.315} ${
+                        height - 20
+                    }C${width * 0.353} ${height - 31} ${width * 0.441} ${height - 104.5} ${width * 0.559} ${
+                        height - 104.5
+                    }C${width * 0.672} ${height - 104.5} ${width * 0.773} ${height + 100} ${width * 1.3} ${
+                        height - 224.5
+                    }C${width * 0.903} ${height - 223.3} ${width * 0.983} ${height - 199.667} ${width} ${
+                        height - 188
+                    }V0H0`;
+                }
+
                 imgDiv.style.clipPath = `path('${path}')`;
             }
         };
