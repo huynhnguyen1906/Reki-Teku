78fac2efd7380a9a53031af1ee591aee4a98b987
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-14 01:56:48 +0900

    refactor: Update global and modal navigation styles
    - Add margin-top to main element in GlobalStyles.scss
    - Remove margin-top from MainVisual class in Index.module.scss
    - Add new SVG file for nav item background
    - Add ModalNavBar component in Header.tsx
    - Add styles for ModalNavBar in ModalNavBar.module.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,21 @@ import logo from '../../../public/images/logo-white.svg';
 import Image from 'next/image';
 import Style from '@styles/componentsStyles/Header.module.scss';
 import '@styles/componentsStyles/CustomNavBar.scss';
+import ModalNavBar from './ModalNavBar';
+import { useState } from 'react';
 
 export default function Header() {
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+
+    const handleClose = () => setShow(false);
     return (
         <header className={Style.Header}>
             <Navbar expand="lg" className={Style.Navbar}>
                 <Navbar.Brand href="/">
                     <Image src={logo} alt="logo" width={45} height={45} />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="d-flex justify-content-end">
                         <Nav.Link href="/about">歴てくについて</Nav.Link>
@@ -22,6 +28,7 @@ export default function Header() {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+            {show && <ModalNavBar handleClose={handleClose} />}
         </header>
     );
 }
