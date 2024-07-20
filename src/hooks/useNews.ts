998e6825b5094e81dcd8df4e92685fb323f92e97
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-20 13:03:13 +0900

    feat: Add revalidation options to useNews and useTour hooks
    - Modified useNews hook to include revalidation options for SWR fetching
      - Added options: revalidateIfStale, revalidateOnFocus, revalidateOnReconnect, revalidateOnMount
    - Modified useTour hook to include revalidation options for SWR fetching
      - Added options: revalidateIfStale, revalidateOnFocus, revalidateOnReconnect, revalidateOnMount

--- src/hooks/useNews.ts
+++ src/hooks/useNews.ts
@@ -4,7 +4,12 @@ import axios from 'axios';
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export const useNews = (id: string) => {
-    const { data, error } = useSWR(`/api/get-news/${id}`, fetcher);
+    const { data, error } = useSWR(`/api/get-news/${id}`, fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+        revalidateOnMount: true,
+    });
 
     return {
         news: data,
--- src/hooks/useTour.ts
+++ src/hooks/useTour.ts
@@ -4,7 +4,12 @@ import axios from 'axios';
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export const useTour = (id: string) => {
-    const { data, error } = useSWR(`/api/get-tour/${id}`, fetcher);
+    const { data, error } = useSWR(`/api/get-tour/${id}`, fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+        revalidateOnMount: true,
+    });
 
     return {
         tour: data,
