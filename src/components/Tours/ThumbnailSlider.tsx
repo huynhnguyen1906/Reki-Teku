5e2906d8a49ad29963a973431a566ef0708a299d
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-22 04:05:29 +0900

    refactor: Update ThumbnailSlider component to use useRef for mainSlider and thumbnailSlider

--- src/components/Tours/ThumbnailSlider.tsx
+++ src/components/Tours/ThumbnailSlider.tsx
@@ -1,55 +1,54 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 import Image from 'next/image';
 import '@styles/componentsStyles/Tours/ThumbnailSlider.scss';
 
 export default function ThumbnailSlider({ schedule }: { schedule: any }) {
-    console.log(schedule);
+    const mainSliderRef = useRef(null);
+    const thumbnailSliderRef = useRef(null);
+
     useEffect(() => {
-        const mainSlider = new Splide('#main-slider', {
-            type: 'fade',
-            rewind: true,
-            pagination: false,
-            arrows: false,
-        });
+        if (mainSliderRef.current && thumbnailSliderRef.current && schedule?.length) {
+            const mainSlider = new Splide(mainSliderRef.current, {
+                type: 'fade',
+                rewind: true,
+                pagination: false,
+                arrows: false,
+            });
 
-        const thumbnailSlider = new Splide('#thumbnail-slider', {
-            fixedWidth: 100,
-            fixedHeight: 60,
-            gap: 10,
-            rewind: true,
-            pagination: false,
-            cover: true,
-            isNavigation: true,
-            breakpoints: {
-                600: {
-                    fixedWidth: 60,
-                    fixedHeight: 44,
+            const thumbnailSlider = new Splide(thumbnailSliderRef.current, {
+                fixedWidth: 100,
+                fixedHeight: 60,
+                gap: 10,
+                rewind: true,
+                pagination: false,
+                cover: true,
+                isNavigation: true,
+                breakpoints: {
+                    600: {
+                        fixedWidth: 60,
+                        fixedHeight: 44,
+                    },
                 },
-            },
-        });
+            });
 
-        mainSlider.sync(thumbnailSlider);
-        mainSlider.mount();
-        thumbnailSlider.mount();
-    }, []);
+            mainSlider.sync(thumbnailSlider);
+            mainSlider.mount();
+            thumbnailSlider.mount();
+        }
+    }, [schedule]);
 
     return (
         <div className="splideContainer">
-            <div id="main-slider" className="splide mainSliderContainer">
+            <div id="main-slider" className="splide mainSliderContainer" ref={mainSliderRef}>
                 <div className="splide__track">
                     <ul className="splide__list mainSlider">
                         {schedule?.map((day: any, dayIndex: number) =>
                             day.destinations.map((destination: any, destIndex: number) => (
                                 <li key={`main-${dayIndex}-${destIndex}`} className="splide__slide">
-                                    <Image
-                                        src={destination.image}
-                                        alt={destination.description}
-                                        width={1000}
-                                        height={1000}
-                                    />
+                                    <Image src={destination.image} alt="alt" width={1000} height={1000} />
                                 </li>
                             )),
                         )}
@@ -57,12 +56,12 @@ export default function ThumbnailSlider({ schedule }: { schedule: any }) {
                 </div>
             </div>
 
-            <div id="thumbnail-slider" className="splide thumbnail-slider">
+            <div id="thumbnail-slider" className="splide thumbnail-slider" ref={thumbnailSliderRef}>
                 <div className="splide__track">
                     <ul className="splide__list thumbnail">
                         {schedule?.map((day: any, dayIndex: number) =>
                             day.destinations.map((destination: any, destIndex: number) => (
-                                <li key={`main-${dayIndex}-${destIndex}`} className="splide__slide">
+                                <li key={`thumb-${dayIndex}-${destIndex}`} className="splide__slide">
                                     <Image
                                         src={destination.image}
                                         alt={destination.description}
