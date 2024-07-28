3a84ec371c5c58c6da12c76e5c74340643ac1cc4
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-28 09:30:56 +0900

    feat: Add new admin pages for company profile, index text, privacy policy, terms and conditions, and tours note
    This commit adds new admin pages for company profile, index text, privacy policy, terms and conditions, and tours note. Each page includes a basic layout with a heading and paragraph content. These pages provide the foundation for managing and editing the corresponding content sections in the admin dashboard.

---
+++ src/app/admin/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import AdminLayout from '../AdminLayout';
+export default function TextContent() {
+    return (
+        <>
+            <AdminLayout>
+                <h1>TextContent</h1>
+                <p>TextContent page content</p>
+            </AdminLayout>
+        </>
+    );
+}
