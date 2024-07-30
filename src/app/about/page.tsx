e6d1b6bcc813f57976f188b3377e9877b4cc9f8f
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-07-30 14:51:41 +0900

    refactor: Update About page content and styles
    This commit updates the content and styles of the About page. It adds dynamic text retrieval using hooks and formats the text with line breaks. The page now displays the lead text, main text, and philosophy text fetched from the backend. Additionally, the profile image and greeting text are also fetched and displayed. The styles for the company profile section are adjusted to improve the layout and responsiveness. This update enhances the user experience and ensures that the About page accurately represents the company's information and values.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,13 @@ import React from 'react';
 import Style from '@styles/appStyles/About.module.scss';
 import Image from 'next/image';
 import MainLayout from '@/components/MainLayout';
+import { useAboutText } from '@/hooks/TextContent/useAboutText';
+import { useCompanyProfile } from '@/hooks/TextContent/useCompanyProfile';
+import { formatTextWithLineBreaks } from '@/utils/formatTextWithLineBreaks ';
 
 export default function About() {
+    const { text, isLoading } = useAboutText();
+    const { profile } = useCompanyProfile();
     useEffect(() => {
         const updateBackground = () => {
             if (window.matchMedia('(max-width: 600px)').matches) {
@@ -26,47 +31,28 @@ export default function About() {
             document.body.style.backgroundImage = 'url(/images/bg.svg)';
         };
     }, []);
+    if (isLoading) return null;
     return (
         <MainLayout>
             <div className={Style.companyWrap}>
                 <div className={Style.content}>
                     <h2 className={Style.ttl}>「歴てく」について</h2>
-                    <h3>自分の目と足で歴史の『サイドストーリー』を探求し人生を深める</h3>
-                    <p>
-                        川が大きな流れから小さく枝分かれしていくように、歴史も枝分かれしていきます。
-                        <br />
-                        「歴てく」は、枝分かれした歴史のサイドストーリーを知る旅をあなたにご提供します。
-                    </p>
+                    <h3>{text.leadText}</h3>
+                    <p dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(text.mainText) }}></p>
                 </div>
                 <div className={Style.content}>
                     <h2 className={Style.ttl}>「歴てく」の理念</h2>
-                    <h3>歴史は人が生きていくためのインスピレーションを与える教科書。</h3>
-                    <p>
-                        いにしえの人物それぞれが歩んだ足跡を訪ね、遺構や文物を目にすると、生き方の助けになる「発見」があります。
-                        <br />
-                        有名、無名の史跡をてくてくと歩き、「今後の人生の糧」や「地域のより深い魅力」を見つけませんか？
-                        <br />
-                        また、地域の過去~現在にわたる史跡を巡り、通史を知れば、その地域の「個性」が見えてきます。
-                        <br />
-                        歴てくは、歴史を通じて人々の生活に新たな視点と価値を提供し、地域の魅力を再発見する旅をご案内します。
-                    </p>
+                    <h3>{text.philosophyLeadText}</h3>
+                    <p dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(text.philosophyText) }}></p>
                 </div>
                 <div className={Style.content}>
                     <h2 className={Style.ttl_}>ごあいさつ</h2>
                     <div className={Style.flex}>
                         <div className={Style.text}>
-                            <p>
-                                モラルの低下が懸念される日本社会。
-                                <br />
-                                歴史に興味を持つことで、先人たちの生き方や失敗から学び、道徳観念を養うことができる。
-                            </p>
-                            <p>
-                                歴史上の人物に焦点を当てたツアーを通じて、彼らの信念や過ちから正しい生き方のヒントを提供し、
-                                社会に道徳を取り戻す一助としたい。
-                            </p>
+                            <p dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(text.greetingText) }}></p>
                         </div>
                         <div className={Style.profile}>
-                            <Image src="/images/profile.png" alt="" width={250} height={330} />
+                            <Image src={text.image} alt={text.greetingText} width={300} height={450} priority />
                             <p>代表取締役 吉丸宣孝</p>
                         </div>
                     </div>
@@ -77,56 +63,55 @@ export default function About() {
                     <dl>
                         <div className={Style.about}>
                             <dt>名称</dt>
-                            <dd>歴てく〜IN・SIDE</dd>
+                            <dd>{profile.name}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>設立</dt>
-                            <dd>2025年1月1日</dd>
+                            <dd>{profile.establishment}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>所在地</dt>
-                            <dd>〒819-0366</dd>
-                            <dd>福岡市西区横浜3丁目27-15</dd>
+                            <dd dangerouslySetInnerHTML={{ __html: formatTextWithLineBreaks(profile.address) }}></dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>取締役</dt>
-                            <dd>吉丸宣孝</dd>
+                            <dd>{profile.director}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>連絡先</dt>
-                            <dd>090-2359-2427</dd>
+                            <dd>{profile.contact}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>営業日</dt>
-                            <dd>10:00~18:00(月・火は定休日)</dd>
+                            <dd>{profile.businessDays}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>資本金</dt>
-                            <dd>1,234,567円</dd>
+                            <dd>{profile.capital}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>資格</dt>
-                            <dd>国内旅行業取扱主任者 国内旅行業務旅程管理者</dd>
+                            <dd>{profile.qualifications}</dd>
                         </div>
                     </dl>
                     <dl>
                         <div className={Style.about}>
                             <dt>加盟団体</dt>
-                            <dd>一般社団法人全国旅行業協会</dd>
+                            <dd>{profile.memberships}</dd>
                         </div>
                     </dl>
                 </div>
