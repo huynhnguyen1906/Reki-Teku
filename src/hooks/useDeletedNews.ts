4ddd50da83440d4656bf5663c85ee0d73b3607bd
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-08-01 00:36:44 +0900

    refactor: Enable lazy loading for images in useAdminNews, useAdminTour, useDeletedNews, useDeletedTours, useIndexNews, and useIndexTour hooks

--- src/hooks/useDeletedNews.ts
+++ src/hooks/useDeletedNews.ts
@@ -1,10 +1,6 @@
 import useSWR, { mutate } from 'swr';
 
-const fetcher = (url: string) => {
-    const timestamp = Date.now();
-    const urlWithTimestamp = `${url}?t=${timestamp}`;
-    return fetch(urlWithTimestamp).then((res) => res.json());
-};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useDeletedNews = () => {
     const { data, error } = useSWR('/api/deleted-news-view', fetcher, {
--- src/hooks/useIndexTour.ts
+++ src/hooks/useIndexTour.ts
@@ -1,10 +1,6 @@
 import useSWR, { mutate } from 'swr';
 
-const fetcher = (url: string) => {
-    const timestamp = Date.now();
-    const urlWithTimestamp = `${url}?t=${timestamp}`;
-    return fetch(urlWithTimestamp).then((res) => res.json());
-};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useIndexTour = () => {
     const { data, error } = useSWR('/api/get-tour-index', fetcher, {
