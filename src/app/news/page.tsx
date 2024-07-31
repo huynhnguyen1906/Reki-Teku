810c913083d4d0be47110c861329752562fc9936
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-08-01 00:19:39 +0900

    refactor: Enable lazy loading for images in TourViewPage component

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,79 +1,9 @@
-'use client';
-import { useState, useEffect } from 'react';
-import MainLayout from '@/components/MainLayout';
-import Style from '@styles/appStyles/NewsView.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useIndexNews } from '@/hooks/useIndexNews';
-import { formatDate } from '@/utils/formatDate';
-import { splitText } from '@/utils/splitText';
+import NewsViewPage from '@/components/News/NewsViewPage';
 
-export default function News() {
-    const { news } = useIndexNews();
-    const [charLimit, setCharLimit] = useState(80);
-    const [headerCharLimit, setHeaderCharLimit] = useState(55);
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth <= 1000) {
-                setCharLimit(55);
-                setHeaderCharLimit(10);
-            } else {
-                setCharLimit(80);
-                setHeaderCharLimit(15);
-            }
-        };
-
-        handleResize();
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
+export default function NewsView() {
     return (
-        <MainLayout>
-            <div className={Style.container}>
-                <h2>ニュース・ブログ一覧</h2>
-                <div className={Style.itemBox}>
-                    {news &&
-                        news.map((item: any) => {
-                            const date = formatDate(item.news_timestamp);
-                            const tagClass = item.news_type === 'ブログ更新' ? Style.blogTag : Style.tourTag;
-
-                            return (
-                                <div key={item.id} className={Style.newsItem}>
-                                    <Link href={`/news/${item.id}`} scroll={true}>
-                                        <div className={Style.newsContent}>
-                                            <p className={Style.thumb}>
-                                                <Image
-                                                    src={item.image.file.url}
-                                                    alt={item.header.text}
-                                                    width={367}
-                                                    height={204}
-                                                    priority
-                                                />
-                                            </p>
-                                            <div className={Style.item}>
-                                                <div className={Style.data}>
-                                                    <p>{date}</p>
-                                                </div>
-                                                <p className={tagClass}>{item.news_type}</p>
-                                            </div>
-                                            <h3 className={Style.newsTtl}>
-                                                {splitText(item.header.text, headerCharLimit)}
-                                            </h3>
-                                            <p className={Style.newsText}>
-                                                {splitText(item.paragraph.text, charLimit)}
-                                            </p>
-                                        </div>
-                                    </Link>
-                                </div>
-                            );
-                        })}
-                </div>
-            </div>
-        </MainLayout>
+        <>
+            <NewsViewPage />
+        </>
     );
 }
---
+++ src/components/Tours/TourViewPage.tsx
@@ -0,0 +1,41 @@
+'use client';
+import MainLayout from '@/components/MainLayout';
+import Style from '@styles/appStyles/ToursView.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useIndexTour } from '@/hooks/useIndexTour';
+import { splitText } from '@/utils/splitText';
+
+export default function TourViewPage() {
+    const { tour } = useIndexTour();
+
+    return (
+        <MainLayout>
+            <div className={Style.container}>
+                <h2>ツアー一覧</h2>
+                <div className={Style.itemBox}>
+                    {tour &&
+                        tour.map((item: any) => (
+                            <div key={item.id} className={Style.tourItem}>
+                                <Link href={`/tours/${item.id}`} scroll={true}>
+                                    <div className={Style.toursContent}>
+                                        <p className={Style.thumb}>
+                                            <Image
+                                                src={item.first_destination_image}
+                                                alt={item.tour_info.name}
+                                                width={367}
+                                                height={204}
+                                                priority
+                                            />
+                                        </p>
+                                        <h3 className={Style.toursTtl}>{item.tour_info.name}</h3>
+                                        <p className={Style.toursText}>{splitText(item.tour_info.description, 100)}</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </MainLayout>
+    );
+}
